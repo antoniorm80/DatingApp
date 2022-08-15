1f6661972f0b3c4c995df6647bcdbb8d2f5582ae
antoniorm80 <antoniorm80@hotmail.com>
2022-08-15 17:56:49 -0500

    Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { AccountService } from './_services/account.service';
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -9,23 +11,42 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'The Dating App';
   users: any;
+  oneAtATime = true;
 
- constructor(private http: HttpClient){}
+
+ constructor(private http: HttpClient, public accountService: AccountService){}
 
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-      // this.http.get('https://localhost:5001/api/users').subscribe(response => {
-      //   this.users = response;
-      // }, error => {
-      //   console.log(error);
-      // });
+  setCurrentUser(){
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    this.accountService.setCurrentUser(user);
+  }
 
-      this.http.get('https://localhost:5001/api/users').subscribe({
-        next: response => this.users = response,
-        error: error => console.log(error)
-      })
-    }
+  // getUsers() {
+  //     // this.http.get('https://localhost:5001/api/users').subscribe(response => {
+  //     //   this.users = response;
+  //     // }, error => {
+  //     //   console.log(error);
+  //     // });
+      
+  //     // // <!-- eCommerce - Postman -->
+  //     // this.http.get('https://dasnube.com/api/productos/stock/4c7a217760a8d3d7e5d8d8974464329b').subscribe({
+  //     //   next: response => this.users = response,
+  //     //   error: error => console.log(error)
+  //     // })
+
+  //     // // <!-- Tyhke Mexico -->
+  //     // this.http.get('https://www.tykhe.com.mx/wp-json/wp/v2/users').subscribe({
+  //     //   next: response => this.users = response,
+  //     //   error: error => console.log(error)
+  //     // })
+
+  //     this.http.get('https://localhost:5001/api/users').subscribe({
+  //       next: response => this.users = response,
+  //       error: error => console.log(error)
+  //     })
+  //   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,31 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    BsDropdownModule,
+    CarouselModule,
+    AccordionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
